1531a747e004ea3c859436a02bac0dfd7de87be5
Craig Rupp <craigrupp@utexas.edu>
2022-08-08 12:41:46 -0500

    update app.use

--- myApp.js
+++ myApp.js
@@ -9,7 +9,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
 
-app.use(express.static(__dirname + '/public'));
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
