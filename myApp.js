5c8a5285d226b4f803b731b8f8a2786d54ce3ba9
Craig Rupp <craigrupp@utexas.edu>
2022-08-08 12:20:39 -0500

    sendFile with __dirname and relative file paths

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 let express = require('express');
 let app = express();
+// We recommend you to use the Node global variable __dirname to calculate the path like this: const absolutePath = __dirname + relativePath/file.ext  
 
 console.log("Hello World");
 
 app.get('/', (req, res) => {
-    res.send("Hello Express");
+    //res.send("Hello Express");
+    res.sendFile(__dirname + '/views/index.html')
 });
 
 
